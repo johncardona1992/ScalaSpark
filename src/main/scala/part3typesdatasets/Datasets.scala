612aaac724008c93fa6d087aac16f0e59a6c9931
john <johncardona1992@gmail.com>
2023-02-22 11:10:56 -0500

    fuck this!

--- src/main/scala/part3typesdatasets/Datasets.scala
+++ src/main/scala/part3typesdatasets/Datasets.scala
@@ -5,10 +5,10 @@ import java.sql.Date
 import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
 import org.apache.spark.sql.functions._
 
-
 object Datasets extends App {
 
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Datasets")
     .config("spark.master", "local")
     .getOrCreate()
@@ -19,93 +19,90 @@ object Datasets extends App {
     .option("inferSchema", "true")
     .load("src/main/resources/data/numbers.csv")
 
-  numbersDF.printSchema()
+  // numbersDF.printSchema()
+  // numbersDF.show()
 
   // convert a DF to a Dataset
   implicit val intEncoder = Encoders.scalaInt
   val numbersDS: Dataset[Int] = numbersDF.as[Int]
+  numbersDS.filter(_ < 100).show()
+  // // dataset of a complex type
+  // // 1 - define your case class
+  // case class Car(
+  //               Name: String,
+  //               Miles_per_Gallon: Option[Double],
+  //               Cylinders: Long,
+  //               Displacement: Double,
+  //               Horsepower: Option[Long],
+  //               Weight_in_lbs: Long,
+  //               Acceleration: Double,
+  //               Year: Date,
+  //               Origin: String
+  //               )
 
-  // dataset of a complex type
-  // 1 - define your case class
-  case class Car(
-                Name: String,
-                Miles_per_Gallon: Option[Double],
-                Cylinders: Long,
-                Displacement: Double,
-                Horsepower: Option[Long],
-                Weight_in_lbs: Long,
-                Acceleration: Double,
-                Year: Date,
-                Origin: String
-                )
+  // // 2 - read the DF from the file
+  // def readDF(filename: String) = spark.read
+  //   .option("inferSchema", "true")
+  //   .json(s"src/main/resources/data/$filename")
 
-  // 2 - read the DF from the file
-  def readDF(filename: String) = spark.read
-    .option("inferSchema", "true")
-    .json(s"src/main/resources/data/$filename")
+  // val carsDF = readDF("cars.json")
 
-  val carsDF = readDF("cars.json")
+  // // 3 - define an encoder (importing the implicits)
+  // import spark.implicits._
+  // // 4 - convert the DF to DS
+  // val carsDS = carsDF.as[Car]
 
-  // 3 - define an encoder (importing the implicits)
-  import spark.implicits._
-  // 4 - convert the DF to DS
-  val carsDS = carsDF.as[Car]
+  // // DS collection functions
+  // numbersDS.filter(_ < 100).show()
 
-  // DS collection functions
-  numbersDS.filter(_ < 100)
+  // // map, flatMap, fold, reduce, for comprehensions ...
+  // val carNamesDS = carsDS.map(car => car.Name.toUpperCase())
 
-  // map, flatMap, fold, reduce, for comprehensions ...
-  val carNamesDS = carsDS.map(car => car.Name.toUpperCase())
-
-  /**
-    * Exercises
-    *
-    * 1. Count how many cars we have
-    * 2. Count how many POWERFUL cars we have (HP > 140)
-    * 3. Average HP for the entire dataset
+  /** // * Exercises // * // * 1. Count how many cars we have // * 2. Count how
+    * many POWERFUL cars we have (HP > 140) // * 3. Average HP for the entire
+    * dataset //
     */
 
-  // 1
-  val carsCount = carsDS.count
-  println(carsCount)
+  // // 1
+  // val carsCount = carsDS.count
+  // println(carsCount)
 
-  // 2
-  println(carsDS.filter(_.Horsepower.getOrElse(0L) > 140).count)
+  // // 2
+  // println(carsDS.filter(_.Horsepower.getOrElse(0L) > 140).count)
 
-  // 3
-  println(carsDS.map(_.Horsepower.getOrElse(0L)).reduce(_ + _) / carsCount)
+  // // 3
+  // println(carsDS.map(_.Horsepower.getOrElse(0L)).reduce(_ + _) / carsCount)
 
-  // also use the DF functions!
-  carsDS.select(avg(col("Horsepower")))
+  // // also use the DF functions!
+  // carsDS.select(avg(col("Horsepower")))
 
+  // // Joins
+  // case class Guitar(id: Long, make: String, model: String, guitarType: String)
+  // case class GuitarPlayer(id: Long, name: String, guitars: Seq[Long], band: Long)
+  // case class Band(id: Long, name: String, hometown: String, year: Long)
 
-  // Joins
-  case class Guitar(id: Long, make: String, model: String, guitarType: String)
-  case class GuitarPlayer(id: Long, name: String, guitars: Seq[Long], band: Long)
-  case class Band(id: Long, name: String, hometown: String, year: Long)
+  // val guitarsDS = readDF("guitars.json").as[Guitar]
+  // val guitarPlayersDS = readDF("guitarPlayers.json").as[GuitarPlayer]
+  // val bandsDS = readDF("bands.json").as[Band]
 
-  val guitarsDS = readDF("guitars.json").as[Guitar]
-  val guitarPlayersDS = readDF("guitarPlayers.json").as[GuitarPlayer]
-  val bandsDS = readDF("bands.json").as[Band]
+  // val guitarPlayerBandsDS: Dataset[(GuitarPlayer, Band)] = guitarPlayersDS.joinWith(bandsDS, guitarPlayersDS.col("band") === bandsDS.col("id"), "inner")
 
-  val guitarPlayerBandsDS: Dataset[(GuitarPlayer, Band)] = guitarPlayersDS.joinWith(bandsDS, guitarPlayersDS.col("band") === bandsDS.col("id"), "inner")
+  // /**
+  //   * Exercise: join the guitarsDS and guitarPlayersDS, in an outer join
+  //   * (hint: use array_contains)
+  //   */
 
-  /**
-    * Exercise: join the guitarsDS and guitarPlayersDS, in an outer join
-    * (hint: use array_contains)
-    */
+  // guitarPlayersDS
+  //   .joinWith(guitarsDS, array_contains(guitarPlayersDS.col("guitars"), guitarsDS.col("id")), "outer")
+  //   .show()
 
-  guitarPlayersDS
-    .joinWith(guitarsDS, array_contains(guitarPlayersDS.col("guitars"), guitarsDS.col("id")), "outer")
-    .show()
+  // // Grouping DS
 
-  // Grouping DS
+  // val carsGroupedByOrigin = carsDS
+  //   .groupByKey(_.Origin)
+  //   .count()
+  //   .show()
 
-  val carsGroupedByOrigin = carsDS
-    .groupByKey(_.Origin)
-    .count()
-    .show()
-
-  // joins and groups are WIDE transformations, will involve SHUFFLE operations
+  // // joins and groups are WIDE transformations, will involve SHUFFLE operations
 
 }
--- src/main/scala/part4sql/SparkSql.scala
+++ src/main/scala/part4sql/SparkSql.scala
@@ -32,6 +32,8 @@ object SparkSql extends App {
   spark.sql("use rtjvm")
   val databasesDF = spark.sql("show databases")
 
+  databasesDF.show()
+
   // transfer tables from a DB to Spark tables
   val driver = "org.postgresql.Driver"
   val url = "jdbc:postgresql://localhost:5432/rtjvm"
@@ -58,69 +60,69 @@ object SparkSql extends App {
     }
   }
 
-  transferTables(List(
-    "employees",
-    "departments",
-    "titles",
-    "dept_emp",
-    "salaries",
-    "dept_manager")
-  )
+  // transferTables(List(
+  //   "employees",
+  //   "departments",
+  //   "titles",
+  //   "dept_emp",
+  //   "salaries",
+  //   "dept_manager")
+  // )
 
-  // read DF from loaded Spark tables
-  val employeesDF2 = spark.read.table("employees")
+  // // read DF from loaded Spark tables
+  // val employeesDF2 = spark.read.table("employees")
 
-  /**
-    * Exercises
-    *
-    * 1. Read the movies DF and store it as a Spark table in the rtjvm database.
-    * 2. Count how many employees were hired in between Jan 1 1999 and Jan 1 2000.
-    * 3. Show the average salaries for the employees hired in between those dates, grouped by department.
-    * 4. Show the name of the best-paying department for employees hired in between those dates.
-    */
+  // /**
+  //   * Exercises
+  //   *
+  //   * 1. Read the movies DF and store it as a Spark table in the rtjvm database.
+  //   * 2. Count how many employees were hired in between Jan 1 1999 and Jan 1 2000.
+  //   * 3. Show the average salaries for the employees hired in between those dates, grouped by department.
+  //   * 4. Show the name of the best-paying department for employees hired in between those dates.
+  //   */
 
-  // 1
-  val moviesDF = spark.read
-    .option("inferSchema", "true")
-    .json("src/main/resources/data/movies.json")
+  // // 1
+  // val moviesDF = spark.read
+  //   .option("inferSchema", "true")
+  //   .json("src/main/resources/data/movies.json")
 
-  moviesDF.write
-    .mode(SaveMode.Overwrite)
-    .saveAsTable("movies")
+  // moviesDF.write
+  //   .mode(SaveMode.Overwrite)
+  //   .saveAsTable("movies")
 
-  // 2
-  spark.sql(
-    """
-      |select count(*)
-      |from employees
-      |where hire_date > '1999-01-01' and hire_date < '2000-01-01'
-    """.stripMargin
-  )
+  // // 2
+  // spark.sql(
+  //   """
+  //     |select count(*)
+  //     |from employees
+  //     |where hire_date > '1999-01-01' and hire_date < '2000-01-01'
+  //   """.stripMargin
+  // )
 
-  // 3
-  spark.sql(
-    """
-      |select de.dept_no, avg(s.salary)
-      |from employees e, dept_emp de, salaries s
-      |where e.hire_date > '1999-01-01' and e.hire_date < '2000-01-01'
-      | and e.emp_no = de.emp_no
-      | and e.emp_no = s.emp_no
-      |group by de.dept_no
-    """.stripMargin
-  )
+  // // 3
+  // spark.sql(
+  //   """
+  //     |select de.dept_no, avg(s.salary)
+  //     |from employees e, dept_emp de, salaries s
+  //     |where e.hire_date > '1999-01-01' and e.hire_date < '2000-01-01'
+  //     | and e.emp_no = de.emp_no
+  //     | and e.emp_no = s.emp_no
+  //     |group by de.dept_no
+  //   """.stripMargin
+  // )
 
-  // 4
-  spark.sql(
-    """
-      |select avg(s.salary) payments, d.dept_name
-      |from employees e, dept_emp de, salaries s, departments d
-      |where e.hire_date > '1999-01-01' and e.hire_date < '2000-01-01'
-      | and e.emp_no = de.emp_no
-      | and e.emp_no = s.emp_no
-      | and de.dept_no = d.dept_no
-      |group by d.dept_name
-      |order by payments desc
-      |limit 1
-    """.stripMargin
-  ).show()
+  // // 4
+  // spark.sql(
+  //   """
+  //     |select avg(s.salary) payments, d.dept_name
+  //     |from employees e, dept_emp de, salaries s, departments d
+  //     |where e.hire_date > '1999-01-01' and e.hire_date < '2000-01-01'
+  //     | and e.emp_no = de.emp_no
+  //     | and e.emp_no = s.emp_no
+  //     | and de.dept_no = d.dept_no
+  //     |group by d.dept_name
+  //     |order by payments desc
+  //     |limit 1
+  //   """.stripMargin
+  // ).show()
 }
