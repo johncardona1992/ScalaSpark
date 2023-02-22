270bc1e28ea9be6f8616e4131da7316de0875d02
john <johncardona1992@gmail.com>
2023-02-21 20:55:25 -0500

    Filter

--- src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -5,7 +5,8 @@ import org.apache.spark.sql.functions.{col, column, expr}
 
 object ColumnsAndExpressions extends App {
 
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("DF Columns and Expressions")
     .config("spark.master", "local")
     .getOrCreate()
@@ -14,12 +15,14 @@ object ColumnsAndExpressions extends App {
     .option("inferSchema", "true")
     .json("src/main/resources/data/cars.json")
 
+  // carsDF.show()
   // Columns
   val firstColumn = carsDF.col("Name")
-
+  
   // selecting (projecting)
   val carNamesDF = carsDF.select(firstColumn)
-
+  // carNamesDF.show()
+  
   // various select methods
   import spark.implicits._
   carsDF.select(
@@ -29,9 +32,9 @@ object ColumnsAndExpressions extends App {
     'Year, // Scala Symbol, auto-converted to column
     $"Horsepower", // fancier interpolated string, returns a Column object
     expr("Origin") // EXPRESSION
-  )
-
-  // select with plain column names
+    )
+    
+    // select with plain column names
   carsDF.select("Name", "Year")
 
   // EXPRESSIONS
@@ -58,7 +61,7 @@ object ColumnsAndExpressions extends App {
   val carsWithKg3DF = carsDF.withColumn("Weight_in_kg_3", col("Weight_in_lbs") / 2.2)
   // renaming a column
   val carsWithColumnRenamed = carsDF.withColumnRenamed("Weight_in_lbs", "Weight in pounds")
-  // careful with column names
+  // careful with column names, use backticks when using expressions over column names with spaces
   carsWithColumnRenamed.selectExpr("`Weight in pounds`")
   // remove a column
   carsWithColumnRenamed.drop("Cylinders", "Displacement")
@@ -73,67 +76,67 @@ object ColumnsAndExpressions extends App {
   val americanPowerfulCarsDF2 = carsDF.filter(col("Origin") === "USA" and col("Horsepower") > 150)
   val americanPowerfulCarsDF3 = carsDF.filter("Origin = 'USA' and Horsepower > 150")
 
-  // unioning = adding more rows
+  // Union = adding more rows
   val moreCarsDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/more_cars.json")
   val allCarsDF = carsDF.union(moreCarsDF) // works if the DFs have the same schema
 
   // distinct values
   val allCountriesDF = carsDF.select("Origin").distinct()
 
-  /**
-    * Exercises
-    *
-    * 1. Read the movies DF and select 2 columns of your choice
-    * 2. Create another column summing up the total profit of the movies = US_Gross + Worldwide_Gross + DVD sales
-    * 3. Select all COMEDY movies with IMDB rating above 6
-    *
-    * Use as many versions as possible
-    */
+  // /**
+  //   * Exercises
+  //   *
+  //   * 1. Read the movies DF and select 2 columns of your choice
+  //   * 2. Create another column summing up the total profit of the movies = US_Gross + Worldwide_Gross + DVD sales
+  //   * 3. Select all COMEDY movies with IMDB rating above 6
+  //   *
+  //   * Use as many versions as possible
+  //   */
 
-  val moviesDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/movies.json")
-  moviesDF.show()
+  // val moviesDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/movies.json")
+  // moviesDF.show()
 
-  // 1
-  val moviesReleaseDF = moviesDF.select("Title", "Release_Date")
-  val moviesReleaseDF2 = moviesDF.select(
-    moviesDF.col("Title"),
-    col("Release_Date"),
-    $"Major_Genre",
-    expr("IMDB_Rating")
-  )
-  val moviesReleaseDF3 = moviesDF.selectExpr(
-    "Title", "Release_Date"
-  )
+  // // 1
+  // val moviesReleaseDF = moviesDF.select("Title", "Release_Date")
+  // val moviesReleaseDF2 = moviesDF.select(
+  //   moviesDF.col("Title"),
+  //   col("Release_Date"),
+  //   $"Major_Genre",
+  //   expr("IMDB_Rating")
+  // )
+  // val moviesReleaseDF3 = moviesDF.selectExpr(
+  //   "Title", "Release_Date"
+  // )
 
-  // 2
-  val moviesProfitDF = moviesDF.select(
-    col("Title"),
-    col("US_Gross"),
-    col("Worldwide_Gross"),
-    col("US_DVD_Sales"),
-    (col("US_Gross") + col("Worldwide_Gross")).as("Total_Gross")
-  )
+  // // 2
+  // val moviesProfitDF = moviesDF.select(
+  //   col("Title"),
+  //   col("US_Gross"),
+  //   col("Worldwide_Gross"),
+  //   col("US_DVD_Sales"),
+  //   (col("US_Gross") + col("Worldwide_Gross")).as("Total_Gross")
+  // )
 
-  val moviesProfitDF2 = moviesDF.selectExpr(
-    "Title",
-    "US_Gross",
-    "Worldwide_Gross",
-    "US_Gross + Worldwide_Gross as Total_Gross"
-  )
+  // val moviesProfitDF2 = moviesDF.selectExpr(
+  //   "Title",
+  //   "US_Gross",
+  //   "Worldwide_Gross",
+  //   "US_Gross + Worldwide_Gross as Total_Gross"
+  // )
 
-  val moviesProfitDF3 = moviesDF.select("Title", "US_Gross", "Worldwide_Gross")
-    .withColumn("Total_Gross", col("US_Gross") + col("Worldwide_Gross"))
+  // val moviesProfitDF3 = moviesDF.select("Title", "US_Gross", "Worldwide_Gross")
+  //   .withColumn("Total_Gross", col("US_Gross") + col("Worldwide_Gross"))
 
-  // 3
-  val atLeastMediocreComediesDF = moviesDF.select("Title", "IMDB_Rating")
-    .where(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6)
+  // // 3
+  // val atLeastMediocreComediesDF = moviesDF.select("Title", "IMDB_Rating")
+  //   .where(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6)
 
-  val comediesDF2 = moviesDF.select("Title", "IMDB_Rating")
-    .where(col("Major_Genre") === "Comedy")
-    .where(col("IMDB_Rating") > 6)
+  // val comediesDF2 = moviesDF.select("Title", "IMDB_Rating")
+  //   .where(col("Major_Genre") === "Comedy")
+  //   .where(col("IMDB_Rating") > 6)
 
-  val comediesDF3 = moviesDF.select("Title", "IMDB_Rating")
-    .where("Major_Genre = 'Comedy' and IMDB_Rating > 6")
+  // val comediesDF3 = moviesDF.select("Title", "IMDB_Rating")
+  //   .where("Major_Genre = 'Comedy' and IMDB_Rating > 6")
 
-  comediesDF3.show
+  // comediesDF3.show
 }
--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -1,26 +1,30 @@
 package part2dataframes
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.types._
 
 object DataSources extends App {
 
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Data Sources and Formats")
     .config("spark.master", "local")
     .getOrCreate()
 
-  val carsSchema = StructType(Array(
-    StructField("Name", StringType),
-    StructField("Miles_per_Gallon", DoubleType),
-    StructField("Cylinders", LongType),
-    StructField("Displacement", DoubleType),
-    StructField("Horsepower", LongType),
-    StructField("Weight_in_lbs", LongType),
-    StructField("Acceleration", DoubleType),
-    StructField("Year", DateType),
-    StructField("Origin", StringType)
-  ))
+  val carsSchema = StructType(
+    Array(
+      StructField("Name", StringType),
+      StructField("Miles_per_Gallon", DoubleType),
+      StructField("Cylinders", LongType),
+      StructField("Displacement", DoubleType),
+      StructField("Horsepower", LongType),
+      StructField("Weight_in_lbs", LongType),
+      StructField("Acceleration", DoubleType),
+      StructField("Year", DateType),
+      StructField("Origin", StringType)
+    )
+  )
 
   /*
     Reading a DF:
@@ -36,6 +40,7 @@ object DataSources extends App {
     .option("path", "src/main/resources/data/cars.json")
     .load()
 
+  // carsDF.show()
   // alternative reading with options map
   val carsDFWithOptionMap = spark.read
     .format("json")
@@ -58,78 +63,78 @@ object DataSources extends App {
     .mode(SaveMode.Overwrite)
     .save("src/main/resources/data/cars_dupe.json")
 
-  // JSON flags
-  spark.read
-    .schema(carsSchema)
-    .option("dateFormat", "YYYY-MM-dd") // couple with schema; if Spark fails parsing, it will put null
-    .option("allowSingleQuotes", "true")
-    .option("compression", "uncompressed") // bzip2, gzip, lz4, snappy, deflate
-    .json("src/main/resources/data/cars.json")
+  // // JSON flags
+  // spark.read
+  //   .schema(carsSchema)
+  //   .option("dateFormat", "YYYY-MM-dd") // couple with schema; if Spark fails parsing, it will put null
+  //   .option("allowSingleQuotes", "true")
+  //   .option("compression", "uncompressed") // bzip2, gzip, lz4, snappy, deflate
+  //   .json("src/main/resources/data/cars.json")
 
-  // CSV flags
-  val stocksSchema = StructType(Array(
-    StructField("symbol", StringType),
-    StructField("date", DateType),
-    StructField("price", DoubleType)
-  ))
+  // // CSV flags
+  // val stocksSchema = StructType(Array(
+  //   StructField("symbol", StringType),
+  //   StructField("date", DateType),
+  //   StructField("price", DoubleType)
+  // ))
 
-  spark.read
-    .schema(stocksSchema)
-    .option("dateFormat", "MMM dd YYYY")
-    .option("header", "true")
-    .option("sep", ",")
-    .option("nullValue", "")
-    .csv("src/main/resources/data/stocks.csv")
+  // spark.read
+  //   .schema(stocksSchema)
+  //   .option("dateFormat", "MMM dd YYYY")
+  //   .option("header", "true")
+  //   .option("sep", ",")
+  //   .option("nullValue", "")
+  //   .csv("src/main/resources/data/stocks.csv")
 
-  // Parquet
-  carsDF.write
-    .mode(SaveMode.Overwrite)
-    .save("src/main/resources/data/cars.parquet")
+  // // Parquet
+  // carsDF.write
+  //   .mode(SaveMode.Overwrite)
+  //   .save("src/main/resources/data/cars.parquet")
 
-  // Text files
-  spark.read.text("src/main/resources/data/sampleTextFile.txt").show()
+  // // Text files
+  // spark.read.text("src/main/resources/data/sampleTextFile.txt").show()
 
-  // Reading from a remote DB
-  val driver = "org.postgresql.Driver"
-  val url = "jdbc:postgresql://localhost:5432/rtjvm"
-  val user = "docker"
-  val password = "docker"
+  // // Reading from a remote DB
+  // val driver = "org.postgresql.Driver"
+  // val url = "jdbc:postgresql://localhost:5432/rtjvm"
+  // val user = "docker"
+  // val password = "docker"
 
-  val employeesDF = spark.read
-    .format("jdbc")
-    .option("driver", driver)
-    .option("url", url)
-    .option("user", user)
-    .option("password", password)
-    .option("dbtable", "public.employees")
-    .load()
+  // val employeesDF = spark.read
+  //   .format("jdbc")
+  //   .option("driver", driver)
+  //   .option("url", url)
+  //   .option("user", user)
+  //   .option("password", password)
+  //   .option("dbtable", "public.employees")
+  //   .load()
 
-  /**
-    * Exercise: read the movies DF, then write it as
-    * - tab-separated values file
-    * - snappy Parquet
-    * - table "public.movies" in the Postgres DB
-    */
+  // /**
+  //   * Exercise: read the movies DF, then write it as
+  //   * - tab-separated values file
+  //   * - snappy Parquet
+  //   * - table "public.movies" in the Postgres DB
+  //   */
 
-  val moviesDF = spark.read.json("src/main/resources/data/movies.json")
+  // val moviesDF = spark.read.json("src/main/resources/data/movies.json")
 
-  // TSV
-  moviesDF.write
-    .format("csv")
-    .option("header", "true")
-    .option("sep", "\t")
-    .save("src/main/resources/data/movies.csv")
+  // // TSV
+  // moviesDF.write
+  //   .format("csv")
+  //   .option("header", "true")
+  //   .option("sep", "\t")
+  //   .save("src/main/resources/data/movies.csv")
 
-  // Parquet
-  moviesDF.write.save("src/main/resources/data/movies.parquet")
+  // // Parquet
+  // moviesDF.write.save("src/main/resources/data/movies.parquet")
 
-  // save to DF
-  moviesDF.write
-    .format("jdbc")
-    .option("driver", driver)
-    .option("url", url)
-    .option("user", user)
-    .option("password", password)
-    .option("dbtable", "public.movies")
-    .save()
+  // // save to DF
+  // moviesDF.write
+  //   .format("jdbc")
+  //   .option("driver", driver)
+  //   .option("url", url)
+  //   .option("user", user)
+  //   .option("password", password)
+  //   .option("dbtable", "public.movies")
+  //   .save()
 }
